19d263c1cf0ff1bc3dbea65e957784ff34826b59
Alex-GF <yalejandro9@gmail.com>
2025-10-03 10:17:56 +0200

    refactor: main page

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ""),
-      }
-    }
-  },
   plugins: [
     nodePolyfills(),
     react(),
@@ -24,15 +15,6 @@ export default defineConfig({
         }
       }
     },
-    {
-      name: "yaml-loader",
-      transform(code, id) {
-        if (id.endsWith(".yml") || id.endsWith(".yaml")) {
-          // For .yml or .yaml files, get the raw content
-          return `export default ${JSON.stringify(code)};`;
-        }
-      }
-    }
   ],
   publicDir: 'public',
 });
